a69e8b4bf0ef6970cbcd6f99508cd10e73e3f15f
Ashwath Soni <ashwathsoni005@gmail.com>
2024-07-09 13:54:27 +0530

    updated app.py to make a mock run

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello from Ashwath!\nHosting this on Vercel using flask framework as a backend. Lets do this team!'
 
 
 if __name__ == '__main__':
