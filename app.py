b2df51c0c3c61e4623fcecc81a57a5d4331c2c80
Ashwath Soni <ashwathsoni005@gmail.com>
2024-07-09 14:45:48 +0530

    trying to fix error 500 again

--- app.py
+++ app.py
@@ -56,6 +56,6 @@ def index():
 #     return render_template('login.html')
 
 if __name__ == '__main__':
-    with app.app_context():
-#db.create_all()
+    #with app.app_context():
+        #db.create_all()
     app.run()
