f5e2c86636ed94ffe1e38e8b54d248f07b38e11e
Ashwath Soni <ashwathsoni005@gmail.com>
2024-07-09 14:50:11 +0530

    trying to fix the internal server error

--- app.py
+++ app.py
@@ -3,59 +3,59 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://default:SA57KMxEhzwb@ep-holy-firefly-a1hm97ya.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#
-# db = SQLAlchemy(app)
-#
-# class User(db.Model):
-#     __tablename__ = 'login'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(50), unique=True, nullable=False)
-#     password = db.Column(db.String(100), nullable=False)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://default:SA57KMxEhzwb@ep-holy-firefly-a1hm97ya.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'login'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if request.method == 'POST':
-#
-#         username = request.form['username']
-#         password = request.form['password']
-#
-#         if User.query.filter_by(username=username).first():
-#             return 'Username is already taken!'
-#
-#         new_user = User(username=username, password=password)
-#
-#         db.session.add(new_user)
-#
-#         db.session.commit()
-#
-#         return redirect(url_for('login'))
-#
-#     return render_template('register.html')
-#
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#
-#         user = User.query.filter_by(username=username).first()
-#
-#         if user and user.password == password:
-#             session['user_id'] = user.id
-#             return redirect(url_for('dashboard'))
-#         else:
-#             return 'Invalid username or password. Please try again.'
-#
-#     return render_template('login.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        if User.query.filter_by(username=username).first():
+            return 'Username is already taken!'
+
+        new_user = User(username=username, password=password)
+
+        db.session.add(new_user)
+
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('dashboard'))
+        else:
+            return 'Invalid username or password. Please try again.'
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    #with app.app_context():
-        #db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run()
